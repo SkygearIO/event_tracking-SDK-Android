25e61728e7697b6e65e1693ac194f9229ab4e722
Louis Chan <louischan@oursky.com>
2017-05-02 19:59:58 +0800

    Implement tracker

---
+++ lib/src/main/java/io/skygear/skygear/eventtracking/SkygearTracker.java
@@ -0,0 +1,112 @@
+package io.skygear.skygear.eventtracking;
+
+import android.content.Context;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.skygear.skygear.Container;
+
+public class SkygearTracker {
+
+    private final static String DEFAULT_MOUNT_PATH = "/skygear_event_tracking";
+
+    private final Container mContainer;
+    private final Map<String, Object> mEnvironmentAttributes;
+    private final Writer mWriter;
+    private final Uri mEndpoint;
+
+    public SkygearTracker(Container container) {
+        this(container, DEFAULT_MOUNT_PATH);
+    }
+
+    public SkygearTracker(Container container, String mountPath) {
+        mContainer = container;
+        mEnvironmentAttributes = new HashMap<>();
+        mEndpoint = buildEndpoint(mountPath);
+        mWriter = new Writer(getContext(), mEndpoint);
+        populateEnvironmentAttributes();
+    }
+
+    public void track(String eventName) {
+        track(eventName, null);
+    }
+
+    public void track(String eventName, Map<String, Object> attributes) {
+        if (eventName == null) {
+            return;
+        }
+
+        HashMap<String, Object> event = new HashMap<>();
+        Map<String, Object> sanitizedAttributes = sanitizeUserDefinedAttributes(attributes);
+        if (sanitizedAttributes != null) {
+            event.putAll(sanitizedAttributes);
+        }
+        event.putAll(mEnvironmentAttributes);
+        event.put("_event_raw", eventName);
+        event.put("_user_id", getCurrentUserId());
+        Date trackedAt = new Date();
+        event.put("_tracked_at", trackedAt);
+        mWriter.write(event);
+    }
+
+    private Context getContext() {
+        return mContainer.getContext();
+    }
+
+    private String getCurrentUserId() {
+        if (mContainer.getCurrentUser() != null) {
+            return mContainer.getCurrentUser().getId();
+        }
+        return null;
+    }
+
+    private Uri buildEndpoint(String mountPath) {
+        Uri base = Uri.parse(mContainer.getConfig().getEndpoint());
+        return base.buildUpon().encodedPath(mountPath).build();
+    }
+
+    private HashMap<String, Object> sanitizeUserDefinedAttributes(Map<String, Object> attributes) {
+        if (attributes == null) {
+            return null;
+        }
+        HashMap<String, Object> output = new HashMap<>();
+        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof Boolean) {
+                output.put(key, value);
+            } else if (value instanceof Number) {
+                Number number = (Number) value;
+                output.put(key, number.doubleValue());
+            } else if (value instanceof String) {
+                output.put(key, value);
+            }
+        }
+        return output;
+    }
+
+    private void populateEnvironmentAttributes() {
+        Context context = getContext();
+        mEnvironmentAttributes.put("_app_id", Utils.getAppId(context));
+        mEnvironmentAttributes.put("_app_version", Utils.getAppVersion(context));
+        mEnvironmentAttributes.put("_app_build_number", Utils.getAppBuildNumber(context));
+        mEnvironmentAttributes.put("_device_id", Utils.getDeviceId(context));
+        mEnvironmentAttributes.put("_device_manufacturer", Utils.getDeviceManufacturer());
+        mEnvironmentAttributes.put("_device_model", Utils.getDeviceModel());
+        mEnvironmentAttributes.put("_device_os", Utils.getDeviceOS());
+        mEnvironmentAttributes.put("_device_os_version", Utils.getDeviceOSVersion());
+        mEnvironmentAttributes.put("_device_carrier", Utils.getDeviceCarrier(context));
+        mEnvironmentAttributes.put("_device_locales", formatBCP47Tags(Utils.getDeviceLocales(context)));
+        mEnvironmentAttributes.put("_device_locale", Utils.getDeviceLocale(context));
+    }
+
+    private String formatBCP47Tags(ArrayList<String> tags) {
+        return TextUtils.join(",", tags);
+    }
+
+}
---
+++ lib/src/main/java/io/skygear/skygear/eventtracking/Writer.java
@@ -0,0 +1,274 @@
+package io.skygear.skygear.eventtracking;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.util.AtomicFile;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+class Writer {
+    private static final String LOGTAG = "SETWriter";
+    private static final String DEFAULT_FILE_PATH = "skygear_event_tracking.json";
+    private static final int DEFAULT_FLUSH_LIMIT = 10;
+    private static final long DEFAULT_TIMER_INTERVAL = 30; // in seconds
+    private static final int DEFAULT_UPLOAD_LIMIT = 20;
+
+    private final Context mContext;
+    private final ExecutorService mExecutor;
+    private final SimpleDateFormat mDateFormatter;
+    private final Uri mEndpoint;
+    private final AtomicFile mFile;
+    private final ScheduledExecutorService mTimer;
+    private ArrayList<HashMap<String, Object>> mEvents;
+
+    public Writer(Context context, Uri endpoint) {
+        mContext = context;
+        mExecutor = Executors.newSingleThreadExecutor();
+        mTimer = Executors.newSingleThreadScheduledExecutor();
+        mDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        mEndpoint = endpoint;
+        mFile = new AtomicFile(getFile());
+        mEvents = new ArrayList<>();
+
+        mExecutor.submit(new Runnable() {
+            @Override
+            public void run() {
+                Writer.this.doRestore();
+            }
+        });
+
+        mTimer.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                Writer.this.flushIfHasSomeEvents();
+            }
+        }, 0L, DEFAULT_TIMER_INTERVAL, TimeUnit.SECONDS);
+    }
+
+    private File getFile() {
+        File dataDir = mContext.getApplicationContext().getFilesDir();
+        File file = new File(dataDir, DEFAULT_FILE_PATH);
+        return file;
+    }
+
+    public void write(final HashMap<String, Object> event) {
+        mExecutor.submit(new Runnable() {
+            @Override
+            public void run() {
+                Writer.this.doWrite(event);
+            }
+        });
+    }
+
+    private void doRestore() {
+        try {
+            byte[] bytes = mFile.readFully();
+            JSONObject jsonObject = Utils.fromBytesTOJSONObject(bytes);
+            JSONArray jsonArray = jsonObject.getJSONArray("events");
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                JSONObject eventJson = jsonArray.getJSONObject(i);
+                HashMap<String, Object> event = fromJSONObject(eventJson);
+                if (event != null) {
+                    mEvents.add(event);
+                }
+            }
+            Log.d(LOGTAG, "doRestore: stored: " + mEvents.size());
+        } catch (Exception e) {
+            if (!(e instanceof FileNotFoundException)) {
+                Log.e(LOGTAG, "doRestore", e);
+            }
+        }
+    }
+
+    private Date parseDateFromJSONObject(JSONObject jsonObject) {
+        try {
+            String type = jsonObject.getString("$type");
+            if ("date".equals(type)) {
+                String rfc3339 = jsonObject.getString("$date");
+                Date date = mDateFormatter.parse(rfc3339);
+                return date;
+            }
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    private JSONObject serializeDateToJSONObject(Date date) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("$type", "date");
+        jsonObject.put("$date", mDateFormatter.format(date));
+        return jsonObject;
+    }
+
+    private HashMap<String, Object> fromJSONObject(JSONObject jsonObject) throws JSONException {
+        HashMap<String, Object> output = new HashMap<>();
+        Iterator<String> iterator = jsonObject.keys();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            Object value = jsonObject.get(key);
+            if (value instanceof Boolean) {
+                output.put(key, value);
+            } else if (value instanceof Number) {
+                output.put(key, ((Number) value).doubleValue());
+            } else if (value instanceof String) {
+                output.put(key, value);
+            } else if (value instanceof JSONObject) {
+                Date date = parseDateFromJSONObject((JSONObject) value);
+                if (date != null) {
+                    output.put(key, date);
+                }
+            }
+        }
+        return output;
+    }
+
+    private JSONObject toJSONObject(HashMap<String, Object> event) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        for (Map.Entry<String, Object> entry : event.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof Boolean) {
+                jsonObject.put(key, (boolean) value);
+            } else if (value instanceof Number) {
+                jsonObject.put(key, (double) value);
+            } else if (value instanceof String) {
+                jsonObject.put(key, (String) value);
+            } else if (value instanceof Date) {
+                jsonObject.put(key, serializeDateToJSONObject((Date) value));
+            }
+        }
+        return jsonObject;
+    }
+
+    private JSONObject serializeEvents(List<HashMap<String, Object>> events) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        for (HashMap<String, Object> event : events) {
+            JSONObject eventJSON = toJSONObject(event);
+            if (eventJSON != null) {
+                jsonArray.put(eventJSON);
+            }
+        }
+        jsonObject.put("events", jsonArray);
+        return jsonObject;
+    }
+
+    private void doWrite(HashMap<String, Object> event) {
+        try {
+            mEvents.add(event);
+            Log.d(LOGTAG, "doWrite: after write: " + mEvents.size());
+            persist();
+            flushIfEnough();
+        } catch (Exception e) {
+            Log.e(LOGTAG, "doWrite", e);
+        }
+    }
+
+    private void persist() throws Exception {
+        JSONObject jsonObject = serializeEvents(mEvents);
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = mFile.startWrite();
+            InputStream inputStream = new ByteArrayInputStream(Utils.fromJSONObjectToBytes(jsonObject));
+            Utils.transferTo(inputStream, outputStream);
+            mFile.finishWrite(outputStream);
+        } catch (Exception e) {
+            mFile.failWrite(outputStream);
+            throw e;
+        }
+    }
+
+    private <T> int calculateEndIndex(ArrayList<T> arrayList, int limit) {
+        int size = arrayList.size();
+        return Math.min(size, limit);
+    }
+
+    private void flushIfEnough() throws IOException, JSONException {
+        if (mEvents.size() < DEFAULT_FLUSH_LIMIT) {
+            return;
+        }
+        flush();
+    }
+
+    private void flushIfHasSomeEvents() {
+        if (mEvents.size() <= 0) {
+            return;
+        }
+        try {
+            flush();
+            Log.d(LOGTAG, "flushIfHasSomeEvents success");
+        } catch (Exception e) {
+            Log.e(LOGTAG, "flushIfHasSomeEvents error", e);
+        }
+    }
+
+    private void flush() throws IOException, JSONException {
+        int endIndex = calculateEndIndex(mEvents, DEFAULT_UPLOAD_LIMIT);
+        List<HashMap<String, Object>> events = mEvents.subList(0, endIndex);
+        JSONObject jsonObject = serializeEvents(events);
+        try {
+            upload(jsonObject);
+            mEvents = new ArrayList<>(mEvents.subList(events.size(), mEvents.size()));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private void upload(JSONObject jsonObject) throws IOException {
+        URL javaURL = Utils.fromUriToURL(mEndpoint);
+        HttpURLConnection urlConnection = null;
+        try {
+            byte[] requestBody = Utils.fromJSONObjectToBytes(jsonObject);
+
+            urlConnection = (HttpURLConnection) javaURL.openConnection();
+            urlConnection.setUseCaches(false);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.setFixedLengthStreamingMode(requestBody.length);
+
+            InputStream requestBodyStream = new ByteArrayInputStream(requestBody);
+            OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
+            Utils.transferTo(requestBodyStream, outputStream);
+            outputStream.close();
+
+            InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+            Utils.readToEnd(inputStream);
+            inputStream.close();
+            int statusCode = urlConnection.getResponseCode();
+            Log.d(LOGTAG, "upload: " + statusCode);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+}
